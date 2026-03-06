1830783fecdea507f863dd3a72fa53f3736031d8
duxinwei-rgb <duxinwei2026@163.com>
2026-03-06 14:51:53 +0800

    第二次提交，添加input输入功能

--- ex01/Helloworld.c
+++ ex01/Helloworld.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+int input() {
+    char data[100];
+    printf("请输入一些数据：");
+    scanf("%s", data);
+    return 0;
+}
 int main() {
     printf("Hello, World!\n");
+    int a=input();
+    printf("你输入的数据是：%s\n", data);
     return 0;
 }
