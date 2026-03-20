df26e4d487534b77f837275a576158c06149a53e
duxinwei-rgb <duxinwei2026@163.com>
2026-03-20 19:51:41 +0800

    四个led以1Hz频率闪烁

--- ex02/led_1hz/led_1hz.ino
+++ ex02/led_1hz/led_1hz.ino
@@ -1,5 +1,8 @@
 const int ledPin=2;
 const int ledPin_R=25;
+const int ledPin_G=33;
+const int ledPin_Y=32;
+
 
 unsigned long previousMillis=0;
 const long interval=1000;
@@ -10,6 +13,8 @@ void setup() {
   Serial.begin(115200);
   pinMode(ledPin,OUTPUT);
   pinMode(ledPin_R,OUTPUT);
+  pinMode(ledPin_G,OUTPUT);
+  pinMode(ledPin_Y,OUTPUT);
 
 }
 
@@ -20,6 +25,9 @@ void loop() {
     ledState=!ledState;
     digitalWrite(ledPin,ledState);
     digitalWrite(ledPin_R,ledState);
+    digitalWrite(ledPin_G,ledState);
+    digitalWrite(ledPin_Y,ledState);
+
 
   }
 
