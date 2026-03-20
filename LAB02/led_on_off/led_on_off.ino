3f58febd05fb75c53cee201909ecb5a2bd23fffe
duxinwei-rgb <duxinwei2026@163.com>
2026-03-20 18:53:45 +0800

    两个灯依次闪烁

--- LAB02/led_on_off/led_on_off.ino
+++ LAB02/led_on_off/led_on_off.ino
@@ -1,10 +1,13 @@
 // 定义LED引脚，ESP32通常板载LED连接在GPIO 2
 const int ledPin = 2; 
+const int ledPin_R = 25; 
+
 void setup() {
   // 初始化串口通信，设置波特率为115200
   Serial.begin(115200);
   // 将LED引脚设置为输出模式
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin_R, OUTPUT);
   
 }
 
@@ -16,6 +19,13 @@ void loop() {
   digitalWrite(ledPin, LOW);   // 熄灭LED
   Serial.println("LED OFF");   // 串口输出提示
   delay(1000);                 // 保持1秒
-  
+
+  digitalWrite(ledPin_R, HIGH);  // 点亮LED
+  Serial.println("LED_R ON");    // 串口输出提示
+  delay(1000);                 // 保持1秒（1000毫秒）
+
+  digitalWrite(ledPin_R, LOW);   // 熄灭LED_R
+  Serial.println("LED_R OFF");   // 串口输出提示
+  delay(1000);                 // 保持1秒
 
 }
