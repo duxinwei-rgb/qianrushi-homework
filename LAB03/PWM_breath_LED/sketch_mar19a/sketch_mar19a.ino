758fdc74ee91606ebd2d951698bded35a0c5d6cc
duxinwei-rgb <duxinwei2026@163.com>
2026-03-20 19:25:53 +0800

    四个led呼吸灯+视频

--- LAB03/PWM_breath_LED/sketch_mar19a/sketch_mar19a.ino
+++ LAB03/PWM_breath_LED/sketch_mar19a/sketch_mar19a.ino
@@ -1,6 +1,8 @@
 // 定义LED引脚
 const int ledPin = 2;  
-
+const int ledPin_R =25;  
+const int ledPin_G = 33;  
+const int ledPin_Y = 32;
 
 // 设置PWM属性
 const int freq = 5000;          // 频率 5000Hz
@@ -8,9 +10,10 @@ const int resolution = 8;       // 分辨率 8位 (0-255)
 
 void setup() {
   Serial.begin(115200);
-
-
   ledcAttach(ledPin, freq, resolution);
+  ledcAttach(ledPin_R, freq, resolution);
+  ledcAttach(ledPin_G, freq, resolution);
+  ledcAttach(ledPin_Y, freq, resolution);
 
 }
 
@@ -18,12 +21,18 @@ void loop() {
   // 逐渐变亮
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
     ledcWrite(ledPin, dutyCycle);   
+    ledcWrite(ledPin_R, dutyCycle); 
+    ledcWrite(ledPin_G, dutyCycle); 
+    ledcWrite(ledPin_Y, dutyCycle);
     delay(10);
   }
 
   // 逐渐变暗
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     ledcWrite(ledPin, dutyCycle); 
+    ledcWrite(ledPin_R, dutyCycle); 
+    ledcWrite(ledPin_G, dutyCycle); 
+    ledcWrite(ledPin_Y, dutyCycle);
 
     delay(10);
   }
